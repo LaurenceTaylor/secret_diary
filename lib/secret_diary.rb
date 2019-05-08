f597a893a50e6fdf9f7d8e69486814e4817864d1
Laurence <ltaylorapp@gmail.com>
2019-05-08 12:54:26 +0100

    Added get_entries and add_entry methods, they raise errors when the diary is locked

--- lib/secret_diary.rb
+++ lib/secret_diary.rb
@@ -11,6 +11,14 @@ class Diary
     @locked = false
   end
 
+  def add_entry
+    raise 'You cannot add entry to a locked diary' if locked?
+  end
+
+  def get_entries
+    raise 'You cannot get entries to a locked diary' if locked?
+  end
+
   def locked?
     @locked
   end
--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -1,19 +1,37 @@
 require 'secret_diary'
 
 describe Diary do
-  context 'Diary is locked' do
+  context 'The diary is locked' do
     before(:each) do
       subject.lock
     end
-    
+
     describe '#unlock' do
       it 'should unlock the diary' do
         expect { subject.unlock }.to change{subject.locked?}.from(true).to(false)
       end
     end
+
+    describe '#add_entry' do
+      it 'should raise an error' do
+        message = 'You cannot add entry to a locked diary'
+        expect { subject.add_entry }.to raise_error message
+      end
+    end
+
+    describe '#get_entries' do
+      it 'should raise an error' do
+        message = 'You cannot get entries to a locked diary'
+        expect { subject.get_entries }.to raise_error message
+      end
+    end
   end
 
-  context 'Diary is unlocked' do
+  context 'The diary is unlocked' do
+    before(:each) do
+      subject.unlock
+    end
+
     describe '#lock' do
       it 'should lock the diary' do
         expect { subject.lock }.to change{subject.locked?}.from(false).to(true)
